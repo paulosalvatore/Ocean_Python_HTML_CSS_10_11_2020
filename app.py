e06550863d1508c41508fbebca3b416f40d3f2c4
Paulo Salvatore <paulo@salvatore.tech>
2020-11-10 15:51:56 -0300

    Código python preparado para receber a escolha e retornar as informações de nome_jogador e premio

--- app.py
+++ app.py
@@ -9,7 +9,22 @@ bp = Blueprint('app', __name__)
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template('index.html')
+    nome_jogador = None
+    premio = None
+    premio_imagem = None
+
+    if request.method == 'POST':
+        nome_jogador = request.form['nome_jogador']
+        escolha = request.form['escolha']
+
+        if escolha == 'futurista':
+            premio = 'um Sabre de Luz'
+            premio_imagem = 'https://i.pinimg.com/originals/f5/72/7b/f5727b17d6e3cb84596deb8f96cbe071.gif'
+        elif escolha == 'medieval':
+            premio = 'uma Espada'
+            premio_imagem = 'https://media.indiedb.com/images/members/5/4384/4383197/profile/Knight.gif'
+
+    return render_template('index.html', nome_jogador=nome_jogador, premio=premio, premio_imagem=premio_imagem)
 
 
 app.register_blueprint(bp)
